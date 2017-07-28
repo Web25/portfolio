c6f107c372954dad413fabc4f357972888cecbc9
felix <felix.resch@web25.org>
2017-07-28 12:10:00 +0200

    Did stuff

--- gulpfile.js
+++ gulpfile.js
@@ -1,11 +1,25 @@
 const gulp = require("gulp"),
     pug = require("gulp-pug"),
-    data = require('./data.json');
+    data = require('./data.json'),
+    rename = require('gulp-rename');
 
 gulp.task("pug", function () {
     return gulp.src("src/*.pug")
         .pipe(pug({locals: data}))
-        .pipe(gulp.dest("dist"))
+        .pipe(gulp.dest("dist"));
 });
 
-gulp.task("default", ["pug"]);
+gulp.task("img", function () {
+    if(data.img) {
+        const img = data.img;
+        data.img = "img/avatar" + img.substring(img.lastIndexOf("."));
+        return gulp.src(data.img)
+            .pipe(rename(function (path) {
+                path.dirname = "img";
+                path.basename = "avatar";
+            }))
+            .pipe(gulp.dest("dist"))
+    }
+});
+
+gulp.task("default", ["img", "pug"]);
